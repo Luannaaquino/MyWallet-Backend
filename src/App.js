d31aa31ef8ee85c7c0c3e443356e13fa05e2debc
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-24 16:32:19 -0300

    sign-up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import express from 'express';
 import cors from 'cors';
+import Joi from "joi";
+import pg from 'pg';
+import bcrypt from 'bcrypt'; //segurança de senha
+
+const databaseConfig = {
+    user: 'postgres',
+    password: '123456',
+    database: 'mywallet',
+    host: 'localhost',
+    port: 5432
+};
+
+const { Pool } = pg;
+const connection = new Pool(databaseConfig);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/teste", (req,res) =>{
-    res.send("Ok")
-})
+//rota cadastro
+app.post('/sign-up', async (req,res) => {
+    const { name, email, password, passwordConfirmation} = req.body;
+
+    const signUpSchema = Joi.object({
+        name: Joi.string().alphanum().min(2).max(20).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(3).max(20).required(),
+        passwordConfirmation: Joi.ref('password'),
+    });
+
+    const validation = signUpSchema.validate(req.body);
+    if(validation.error){
+        return res.sendStatus(422)
+    }
+
+    const checkEmail = await connection.query(`
+    SELECT * FROM users 
+    WHERE email = $1`,[email]);
+    if(checkEmail.rows.length !==0){
+        return res.sendStatus(409)
+    }
+
+    const hashPassword = bcrypt.hashSync(password,12)
+    await connection.query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3)`, [name, email, hashPassword]);
+
+    res.sendStatus(201)
+    
+});
+    
+    
 
 app.listen(4000, () => {
     console.log('Server running on port 4000')
---
+++ src/schemas/userSchemas.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+const signIn = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
+
+const signUp = Joi.object({
+    username: Joi.string().alphanum().min(2).max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).max(20).required(),
+    passwordConfirmation: Joi.ref('password'),
+});
+
+
+export default signUp;
