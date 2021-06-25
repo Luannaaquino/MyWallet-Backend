e6d68cb68667a37c395314e7bcba78070b96ec5c
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-25 15:55:31 -0300

    test sing-up

--- src/test/users.test.js
+++ src/test/users.test.js
@@ -25,5 +25,30 @@ describe('POST /sign-up', () => {
         const result = await supertest(app).post('/sign-up').send(body);
 
         expect(result.status).toEqual(201)
-    })
+    });
+
+    it('return status 422 for invalid params', async () => {
+        const body = {
+            name: "teste", 
+            email: "teste@teste.com", 
+        }
+
+        const result = await supertest(app).post('/sign-up').send(body);
+
+        expect(result.status).toEqual(422)
+    });
+
+    it('return status 409 for duplicated params', async () => {
+        const body = {
+            name: "teste", 
+            email: "teste2@teste.com", 
+            password:"123456", 
+            passwordConfirmation:"123456"
+        }
+
+        await supertest(app).post('/sign-up').send(body);
+        const result = await supertest(app).post('/sign-up').send(body);
+
+        expect(result.status).toEqual(409)
+    });
 })
