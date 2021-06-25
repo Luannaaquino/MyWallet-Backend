e8b22a9029829a472e0896eb79d56ca125c7c517
Luanna Aquino <luannaluleticia@gmail.com>
2021-06-25 11:52:05 -0300

    sign-in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import Joi from "joi";
 import pg from 'pg';
 import bcrypt from 'bcrypt'; //segurança de senha
+import { v4 as uuidv4 } from 'uuid'; //gerar token
 
 const databaseConfig = {
     user: 'postgres',
@@ -21,7 +22,7 @@ app.use(express.json());
 
 //rota cadastro
 app.post('/sign-up', async (req,res) => {
-    const { name, email, password, passwordConfirmation} = req.body;
+    const { name, email, password, passwordConfirmation } = req.body;
 
     const signUpSchema = Joi.object({
         name: Joi.string().alphanum().min(2).max(20).required(),
@@ -49,7 +50,39 @@ app.post('/sign-up', async (req,res) => {
     
 });
     
+//rota Login
+app.post('/sign-in', async (req,res) => {
+    const { email, password } = req.body;
+  
+    const signInSchema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(3).max(20).required(),
+    });
+
+    const validation = signInSchema.validate(req.body);
+    if(validation.error){
+        return res.sendStatus(422)
+    }
+
+    const checkEmail = await connection.query(`
+    SELECT * FROM users 
+    WHERE email = $1`,[email]);
+    if(checkEmail.rows.length !==0){
+        return res.sendStatus(401)
+    }
     
+    if (!bcrypt.compareSync(password, checkEmail.password)){
+        return res.sendStatus(401)
+    }
+
+    const token = uuidv4();
+    await connection.query(`INSERT INTO sessions ("userId", token) VALUES ($1,$2)`, [checkEmail.id, token]);
+
+    const returnUser = { id: checkEmail.id, name: checkEmail.name, email: checkEmail.email, token}
+
+    res.sendStatus(returnUser)
+    
+});
 
 app.listen(4000, () => {
     console.log('Server running on port 4000')
